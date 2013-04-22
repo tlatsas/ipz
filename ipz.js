74e6d5b67f889d8ae522bdbdabd5c2848d3d823c
Tasos Latsas <tlatsas2000@gmail.com>
2013-04-22 21:08:42 +0300

    return hostname when using json content type

--- ipz.js
+++ ipz.js
@@ -1,24 +1,35 @@
 var http = require('http');
+var dns = require('dns');
 
 var server = http.createServer(function(req, res) {
     if (req.method === 'GET') {
-        switch (req.headers.accept) {
-            case 'application/json':
-                res.writeHead(200, {'content-type': 'application/json'});
-                res.write(JSON.stringify({'ip': req.headers['x-forwarded-for']}));
-                res.end();
-                break;
-
-            default:
-                res.writeHead(200, {'content-type': 'text/plain' });
-                res.write(req.headers['x-forwarded-for']);
-                res.end();
-        }
+        ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        dns.reverse(ip, handleResponse);
     }
     else {
         // not implemented
         res.writeHeader(501);
         res.end();
+    }
+
+    function handleResponse(error, domains) {
+        switch (req.headers.accept) {
+            case 'application/json':
+                var data = {'ip': ip};
+                if (!error && domains.length > 1){
+                    data.hostname = domains[0];
+                }
+
+                res.writeHead(200, {'content-type': 'application/json'});
+                res.write(JSON.stringify(data));
+                res.end();
+                break;
+
+            default:
+                res.writeHead(200, {'content-type': 'text/plain' });
+                res.write(ip);
+                res.end();
+        }
     }
 });
 
